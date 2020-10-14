394b10899e61df563bafb5e27408ee1a501d1b9b
fandongling <fandongling@bytedance.com>
2020-10-14 21:02:10 +0800

    add bench func
    Change-Id: Id880750ef54fc7fd17c5e53807fc62237a3a65ee

---
+++ pool.go
@@ -0,0 +1,131 @@
+package pool
+
+import (
+	"errors"
+	"math"
+	"sync"
+	"time"
+)
+
+//task handler
+
+type Handler func()
+
+// runtime worker num config when u need
+type RuntimeWorkerNumFunc func() int
+
+var (
+	ERR_FULL    = errors.New("queue full")
+	ERR_CLOSED  = errors.New("queue closed")
+	ERR_TIMEOUT = errors.New("timeout")
+
+	_WORK_ID int64
+)
+
+type WorkerPool struct {
+	taskQueue            chan Handler
+	exit                 chan struct{}
+	capCnt               int
+	lock                 sync.Mutex
+	workers              []*worker
+	runtimeWorkerNumFunc RuntimeWorkerNumFunc
+}
+
+func NewWorkerPool(capCnt, startWorkerNum int) *WorkerPool {
+	return NewWorkerPoolWithRuntimeWorkerNum(capCnt, startWorkerNum, nil)
+}
+
+func NewWorkerPoolWithRuntimeWorkerNum(capCnt, startWorkerNum int, runtimeWorkerNumFunc RuntimeWorkerNumFunc) *WorkerPool {
+	workerPool := &WorkerPool{
+		exit:      make(chan struct{}),
+		capCnt:    capCnt,
+		taskQueue: make(chan Handler, capCnt),
+	}
+	for i := 0; i < startWorkerNum; i++ {
+		workerPool.addWorker()
+	}
+	workerPool.runtimeWorkerNumFunc = runtimeWorkerNumFunc
+	if workerPool.runtimeWorkerNumFunc != nil {
+		go workerPool.workerSentinel()
+	}
+	return workerPool
+}
+
+func (p *WorkerPool) AddTaskSync(h Handler) error {
+	return p.AddTaskWithTimout(h, 0)
+}
+
+func (p *WorkerPool) AddTaskWithTimout(h Handler, timeout time.Duration) error {
+	if timeout == 0 {
+		select {
+		case <-p.exit:
+			return ERR_CLOSED
+		case p.taskQueue <- h:
+			return nil
+		}
+	} else {
+		select {
+		case <-p.exit:
+			return ERR_CLOSED
+		case p.taskQueue <- h:
+			return nil
+		case <-time.After(timeout):
+			return ERR_TIMEOUT
+		}
+	}
+
+}
+
+func (p *WorkerPool) Close() {
+	p.lock.Lock()
+	defer p.lock.Unlock()
+	close(p.exit)
+	for _, worker := range p.workers {
+		worker.close()
+	}
+	//TODO taskQueue task should return faields or continue process all?
+}
+
+func (p *WorkerPool) Name() string {
+	return "WorkerPool"
+}
+
+func (p *WorkerPool) workerSentinel() {
+	ticker := time.NewTicker(time.Second)
+	for range ticker.C {
+		runtimeWorkerNum := p.runtimeWorkerNumFunc()
+		if runtimeWorkerNum == len(p.workers) {
+			continue
+		}
+		p.lock.Lock()
+		diffNum := runtimeWorkerNum - len(p.workers)
+		adjustNum := int(math.Abs(float64(diffNum)))
+		p.adjustWorkers(adjustNum, diffNum > 0)
+		p.lock.Unlock()
+		//fmt.Printf("WOKER NUM %d diffNum %d \n", len(p.workers), diffNum)
+	}
+}
+
+func (p *WorkerPool) adjustWorkers(cnt int, isAdd bool) {
+	for i := 0; i < cnt; i++ {
+		if isAdd {
+			p.addWorker()
+		} else {
+			p.deleteWorker()
+		}
+	}
+}
+
+func (p *WorkerPool) addWorker() {
+	worker := newWorker()
+	go worker.working(p.taskQueue)
+	p.workers = append(p.workers, worker)
+}
+
+func (p *WorkerPool) deleteWorker() {
+	if len(p.workers) > 0 {
+		worker0 := p.workers[0]
+		worker0.close()
+		p.workers = p.workers[1:]
+	}
+}
--- pool_test.go
+++ pool_test.go
@@ -4,9 +4,19 @@ import (
 	"context"
 	"fmt"
 	"math/rand"
+	"runtime"
+	"sync"
 	"testing"
 	"time"
 )
+
+const benchmarkTimes = 10000
+
+func testFunc() {
+	for i := 0; i < benchmarkTimes; i++ {
+		rand.Intn(benchmarkTimes)
+	}
+}
 
 func TestWorker(t *testing.T) {
 	pool := NewWorkerPoolWithRuntimeWorkerNum(2, 2, nextNum())
@@ -17,6 +27,26 @@ func TestWorker(t *testing.T) {
 		pool.AddTaskSync(h)
 	}
 	time.Sleep(time.Minute)
+}
+
+func BenchmarkPool(b *testing.B) {
+	fmt.Println(runtime.GOMAXPROCS(0))
+	cN := runtime.GOMAXPROCS(0)
+	p := NewWorkerPool(cN, cN)
+	var wg sync.WaitGroup
+	b.ReportAllocs()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		wg.Add(benchmarkTimes)
+		for j := 0; j < benchmarkTimes; j++ {
+			p.AddTaskSync(
+				func() {
+					testFunc()
+					wg.Done()
+				})
+		}
+		wg.Wait()
+	}
 }
 
 func genHandler(ctx context.Context) <-chan func() {
